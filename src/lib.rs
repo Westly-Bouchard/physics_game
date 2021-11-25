66a26c72b824a4d27991ddcc50eaec0d2d975547
Westly Bouchard <westly.bouchard@gmail.com>
2021-11-25 13:21:44 -0700

    Initial Commit and Bevy setup

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+use wasm_bindgen::prelude::*;
+
+pub fn run() {
+    let mut app = App::build();
+
+    app.add_plugins(DefaultPlugins);
+
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+
+    app.run();
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    physics_game::run();
+}
